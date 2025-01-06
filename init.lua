68212277e4e61ab639106ba51106d6d43a987648
Will Corvo <nick.neytron@yandex.ru>
2025-01-06 06:36:32 +0300

    initial commit

---
+++ init.lua
@@ -0,0 +1,17 @@
+--  Base?  --
+
+require('core.options')
+
+--  Core   --
+
+require('core.plugins')
+require('core.mappings')
+require('core.colors')
+
+-- Plugins --
+
+require('plugins.neotree')
+require('plugins.treesitter')
+require('plugins.lsp')
+require('plugins.cmp')
+require('plugins.autopairs')
